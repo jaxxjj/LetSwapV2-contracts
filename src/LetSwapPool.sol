bac050dcfdb15250fad22d663f2450ba3d677c6c
Jackson Chen <yc5082@nyu.edu>
2024-11-29 20:37:30 -0500

    add position

--- src/LetSwapPool.sol
+++ src/LetSwapPool.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT 
-pragma solidity ^0.8.20;
+pragma solidity 0.8.20;
 import {Tick} from "./lib/Tick.sol";
 import {TickMath} from "./lib/TickMath.sol";
 // slot 0 = 32 bytes
--- src/lib/Tick.sol
+++ src/lib/Tick.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT 
-pragma solidity ^0.8.20;
+pragma solidity 0.8.20;
 
 library Tick {
     function tickSpacingToMaxLiquidityPerTick(int24 tickSpacing) internal pure returns (uint128) {
