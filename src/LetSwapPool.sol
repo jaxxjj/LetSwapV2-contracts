41bd0452ffd548bc8004ec4f61b155496b7d96cf
Jackson Chen <yc5082@nyu.edu>
2024-11-29 19:58:27 -0500

    add tickmath

--- src/LetSwapPool.sol
+++ src/LetSwapPool.sol
@@ -1,6 +1,23 @@
 // SPDX-License-Identifier: MIT 
 pragma solidity ^0.8.20;
 import {Tick} from "./lib/Tick.sol";
+import {TickMath} from "./lib/TickMath.sol";
+// slot 0 = 32 bytes
+// 2**256 = 32 bytes
+struct Slot0 {
+    // 160 / 8 = 20 bytes
+    uint160 sqrtPriceX96;
+    // 24 / 8 = 3 bytes
+    int24 tick;
+    // 1 byte
+    bool unlocked;
+}
+
+function checkTicks(int24 tickLower, int24 tickUpper) pure {
+    require(tickLower < tickUpper);
+    require(TickMath.MIN_TICK <= tickLower);
+    require(tickUpper <= TickMath.MAX_TICK);
+}
 contract LetSwapPool {
     address public immutable token0;
     address public immutable token1;
